f9f50df5c6198dace13d7e63ef4970e878037be7
trepichio <trepichio@gmail.com>
2021-05-13 17:12:52 -0300

    init (server,  pub/api/admin routes, auth, config)

---
+++ src/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const x = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+  },
+};
+module.exports = x;
