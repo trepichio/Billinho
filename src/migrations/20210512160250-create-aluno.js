59341fc9472bb45dfe0319eb8b9f4f64be0a3f1d
trepichio <trepichio@gmail.com>
2021-05-15 19:24:20 -0300

    add migration Aluno

---
+++ src/migrations/20210512160250-create-aluno.js
@@ -0,0 +1,47 @@
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Alunos', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    nome: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    CPF: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    dataNascimento: {
+      type: Sequelize.DATEONLY,
+    },
+    celular: {
+      type: Sequelize.BIGINT,
+    },
+    genero: {
+      type: Sequelize.ENUM,
+      values: ['M', 'F'],
+      allowNull: false,
+    },
+    formaPagamento: {
+      type: Sequelize.ENUM,
+      values: ['Boleto', 'Cartão'],
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Alunos'),
+};
