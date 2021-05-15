b9539fc2eec8882a791ae9f1eeb7e153137805ea
trepichio <trepichio@gmail.com>
2021-05-15 19:39:14 -0300

    add migration Matricula

---
+++ src/migrations/20210512185230-create-matricula.js
@@ -0,0 +1,53 @@
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Matriculas', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    valorTotal: {
+      type: Sequelize.DECIMAL(10, 2),
+      allowNull: false,
+    },
+    quantidadeFaturas: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    diaVencimento: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    nomeCurso: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    instituicaoId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Instituicoes',
+        key: 'id',
+      },
+      allowNull: false,
+    },
+    alunoId: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'Alunos',
+        key: 'id',
+      },
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Matriculas'),
+};
