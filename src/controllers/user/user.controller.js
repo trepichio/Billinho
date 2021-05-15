d687b350ee7e4b9539ebea666019ee6f0f69a32c
trepichio <trepichio@gmail.com>
2021-05-15 20:12:03 -0300

    add query for page and limit on GET AllUsers

--- src/controllers/user/user.controller.js
+++ src/controllers/user/user.controller.js
@@ -6,8 +6,8 @@ import { successResponse, errorResponse, uniqueId } from '../../helpers';
 
 export const allUsers = async (req, res) => {
   try {
-    const page = req.params.page || 1;
-    const limit = 2;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 2;
     const users = await User.findAndCountAll({
       order: [['createdAt', 'DESC'], ['firstName', 'ASC']],
       offset: (page - 1) * limit,
