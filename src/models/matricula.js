5f744f345f807a55377c85e63eba0621d44b91a1
trepichio <trepichio@gmail.com>
2021-05-15 20:03:15 -0300

    add hook to create faturas after new Matricula is saved

--- src/models/matricula.js
+++ src/models/matricula.js
@@ -47,6 +47,48 @@ module.exports = (sequelize, DataTypes) => {
     },
   }, {});
 
+  const queryInterface = sequelize.getQueryInterface();
+
+  const Fatura = sequelize.model('Fatura');
+
+  async function createFaturas(instance, options) {
+    const { valorTotal, quantidadeFaturas, diaVencimento } = instance;
+
+    const valorFatura = valorTotal / quantidadeFaturas;
+
+    const now = DateTime.now();
+
+    const vencimento = now.set({ day: diaVencimento });
+
+    const firstPayment = diaVencimento <= now.day
+      ? vencimento.plus({ months: 1 })
+      : vencimento;
+
+
+    const nextPayment = (() => {
+      const THIS = function () {
+        THIS.date = THIS.date ? THIS.date.plus({ months: 1 }) : firstPayment;
+        return THIS;
+      };
+      return THIS;
+    })();
+
+    const faturas = [];
+
+    for (let i = 0; i < quantidadeFaturas; i++) {
+      faturas[i] = {
+        valor: valorFatura,
+        diaVencimento: nextPayment().date.toISODate(),
+        matriculaId: instance.id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+    }
+
+    return await queryInterface.bulkInsert('Faturas', faturas);
+  }
+
+  Matricula.addHook('afterCreate', createFaturas);
   Matricula.associate = function (models) {
     // associations can be defined here
   };
