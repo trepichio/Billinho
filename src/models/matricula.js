e10f7c93ec691c96fca04f69d08db52f3efce4d4
trepichio <trepichio@gmail.com>
2021-05-16 23:15:41 -0300

    add hook to validate before update & clean bills 'open'

--- src/models/matricula.js
+++ src/models/matricula.js
@@ -56,7 +56,6 @@ module.exports = (sequelize, DataTypes) => {
     deleteAt: 'deleteAt',
     hooks: {
       afterDestroy: async (instance, options) => {
-        console.log('entrou matricula');
         const faturas = await instance.getFatura();
 
         for (const fatura of faturas) {
@@ -66,13 +65,51 @@ module.exports = (sequelize, DataTypes) => {
       afterRestore: (instance, options) => {
         instance.getFatura({ paranoid: false }).then(fatura => fatura.restore());
       },
-
     },
   });
 
   const queryInterface = sequelize.getQueryInterface();
 
   Matricula.addHook('afterCreate', (instance, options) => createFaturas(instance, options, queryInterface));
+
+  Matricula.addHook('afterUpdate', (instance, options) => createFaturas(instance, options, queryInterface));
+
+  Matricula.addHook('beforeUpdate', async (instance, options) => {
+    const { valorTotal, quantidadeFaturas } = instance;
+
+    const bills = await instance.getFatura();
+
+    const openBills = bills.filter(bill => bill.status !== 'Paga');
+
+    const paidBills = bills.filter(bill => bill.status === 'Paga');
+
+    const aggPaid = paidBills.reduce((acc, curr, index) => {
+      acc.totalPago += parseFloat(curr.valor);
+      return acc;
+    }, { totalPago: 0 });
+
+    if (valorTotal < aggPaid.totalPago) {
+      throw new Error('Valor do curso não pode ser inferior às faturas já pagas.');
+    }
+
+    if (quantidadeFaturas < paidBills.length) {
+      throw new Error('Quantidade de faturas inválida. Informe um valor superior à quantia de faturas já pagas');
+    }
+
+    if (quantidadeFaturas === paidBills.length && valorTotal !== aggPaid.totalPago) {
+      throw new Error('Você deve informar uma quantidade maior de faturas, que execeda a quantidade de faturas já pagas');
+    }
+
+    if (valorTotal === aggPaid.totalPago && (quantidadeFaturas !== paidBills.length)) {
+      throw new Error('Com os valores providos, parece que o curso já foi todo pago. Portanto, só posso atualizar a quantidade de faturas pela mesma quantidade de faturas já pagas. Por favor, verifique e forneça os valores corretos.');
+    }
+
+    for (const openBill of openBills) {
+      openBill.destroy({ force: true });
+    }
+
+    console.log('allow update and possible creation of new bills');
+  });
   Matricula.associate = function (models) {
     // associations can be defined here
 
